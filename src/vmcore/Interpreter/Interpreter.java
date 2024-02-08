0303df5937ea558d30dcd4441d36deadf63b9bf2
Hysteryx Théo.Sicot <theo.sicot.dev@gmail.com>
2024-02-08 17:37:12 +0100

    Debut de la classe rien d'intéréssant

--- src/vmcore/Interpreter/Interpreter.java
+++ src/vmcore/Interpreter/Interpreter.java
@@ -1,5 +1,23 @@
 package vmcore.Interpreter;
 
+import vmcore.Memory.MemoryCell;
+
 public class Interpreter {
     
+    private File file; 
+
+    public Interpreter() {
+        this.file = new File(); 
+    }
+
+    public void Enfile(MemoryCell mem) {
+        this.file.Enfile(mem);
+    } 
+
+    public void CalculNextInstruction() {
+        MemoryCell inst = this.file.Defile();
+        if (inst != null) {
+            System.out.println("aled");
+        }
+    }
 }
