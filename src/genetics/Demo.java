f70d17ea9eb70d241a20d23f6b5f3e270057e70a
Tom Rousee <tom.rousee@etu.unicaen.fr>
2024-02-20 15:50:05 +0100

    Rework OnePivot

--- src/genetics/Demo.java
+++ src/genetics/Demo.java
@@ -5,17 +5,16 @@ import genetics.crossover.OnePivot;
 
 public class Demo {
     public static void main(String[] args) {
-        Seed seed = new Seed(10);
-        System.out.println(seed.getRedcode());
+        Seed seed = new Seed(15);
+        System.out.println(seed);
 
         Seed seed2 = new Seed(10);
-        System.out.println(seed2.getRedcode());
+        System.out.println(seed2);
 
         OnePivot pivot = new OnePivot(seed, seed2);
 
         Seed child = pivot.getChild();
-        System.out.println(child.getRedcode());
 
-        
+        System.out.println(child.getRedcode());
     }
 }
--- src/genetics/crossover/OnePivot.java
+++ src/genetics/crossover/OnePivot.java
@@ -5,20 +5,22 @@ import genetics.seed.Seed;
 import java.util.Random;
 
 public class OnePivot {
-    private String best, loser;
+    private String seed1, seed2;
     private Random random;
     
-    public OnePivot(Seed best, Seed loser) {
-        this.best = best.toString();
-        this.loser = loser.toString();
+    public OnePivot(Seed seed1, Seed seed2) {
+        this.seed1 = seed1.toString();
+        this.seed2 = seed2.toString();
         this.random = new Random();
     }
 
     public Seed getChild() {
         StringBuilder sb = new StringBuilder();
-        int pivot = (this.random.nextInt(best.length()/8)+1)*8;
-        sb.append(best.substring(0, pivot));
-        sb.append(loser.substring(pivot, loser.length()));
+        int pivot1 = (this.random.nextInt(seed1.length()/8)+1)*8;
+        int pivot2 = (this.random.nextInt(seed2.length()/8)+1)*8;
+        int seed2L = seed2.length();
+        sb.append(seed1.substring(0, pivot1));
+        sb.append(seed2.substring(pivot2, ((pivot1+seed2.length()-pivot2)/8)>15?pivot2+pivot1-seed2L:seed2L));
         return new Seed(sb.toString());
     }
 }
